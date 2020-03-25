75e4649504c27846b2461ed41c11e46cf550b104
Yuri Antishev <ant_ustas@mail.ru>
2020-03-25 11:59:32 +0300

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const app = express();
 
 connectDB();
 
-app.use(express.json({extended: false}));
-
-app.get('/', (req, res) => res.json({msg:'Good Day from miniCRM'}));
+app.use(express.json({ extended: false }));
 
 app.use('/api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+  
+  };
 
 const PORT = process.env.PORT || 5000;
 
